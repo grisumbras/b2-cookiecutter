0f7e2263bdbf5641c8ee80732a2740045a7bddca
Архипов Дмитрий <grisumbras@gmail.com>
2018-11-30 00:38:21 +0300

    git initialization

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import subprocess
 
 
 def project_type():
@@ -11,5 +12,20 @@ def project_type():
         os.remove(os.path.join('src', '{{cookiecutter.project_namespace}}.cpp'))
 
 
+def git():
+    if not {{cookiecutter.git}}:
+        return
+
+    subprocess.call(['git', 'init'])
+
+    remote = 'git@{server}:{owner}/{project}.git'.format(
+        server="{{cookiecutter.remote_server}}",
+        owner="{{cookiecutter.remote_username}}",
+        project="{{cookiecutter.project_slug}}",
+    )
+    subprocess.call(['git', 'remote', 'add', 'origin', remote])
+
+
 if __name__ == '__main__':
     project_type()
+    git()
